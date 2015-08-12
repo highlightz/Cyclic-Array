85e26e5c393ed3e02d211d542563d12b7308e9db
Highlight Clark <highlightz@163.com>
2015-08-12 16:21:43 +0800

    Create DataCache.h

---
+++ DataCache.h
@@ -0,0 +1,33 @@
+#ifndef DATACACHE_H
+#define DATACACHE_H
+
+const int CAPACITY = 5;
+
+// The templates for an associated DataInfo and DataCache must match in DataType
+template < class DataType >
+struct DataInfo
+{
+    int numDatas;
+    DataType datas[ CAPACITY ];
+};
+
+template < class DataType >
+class DataCache
+{
+public:
+    DataCache( );
+    void writeData( const DataType& newData );
+    DataType getLastData( ) const;
+    // getLastDatas returns up to CAPACITY datas in a DataInfo struct;
+    // the number of datas is also in the DataInfo struct.
+    // Datas in the datas array are stored in order, with the latest data first.
+    DataInfo< DataType > getLastDatas( ) const;
+private:
+    int numDatas;  // The number of datas stored in the array
+    int lastIndex;  // The index of the last data that was stored
+    DataType lastDatas[ CAPACITY ];  // Saves up to CAPACITY datas
+};
+
+#include "DataCache.cpp"
+
+#endif // DATACACHE_H
