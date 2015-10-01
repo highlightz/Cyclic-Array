25eff82e76c3901e67b503656eaa0df304e58a2e
Highlight Clark <highlightz@163.com>
2015-10-01 09:48:00 +0800

    Update DataCache.h

--- DataCache.h
+++ DataCache.h
@@ -4,14 +4,14 @@
 //const int CAPACITY = 5;
 
 // The templates for an associated DataInfo and DataCache must match in DataType
-template < class DataType,  int CAPACITY >
+template < class DataType, int CAPACITY >
 struct DataInfo
 {
     int numDatas;
     DataType datas[ CAPACITY ];
 };
 
-template < class DataType >
+template < class DataType, int CAPACITY >
 class DataCache
 {
 public:
@@ -21,7 +21,7 @@ public:
     // getLastDatas returns up to CAPACITY datas in a DataInfo struct;
     // the number of datas is also in the DataInfo struct.
     // Datas in the datas array are stored in order, with the latest data first.
-    DataInfo< DataType > getLastDatas( ) const;
+    DataInfo< DataType, CAPACITY > getLastDatas( ) const;
 private:
     int numDatas;  // The number of datas stored in the array
     int lastIndex;  // The index of the last data that was stored
