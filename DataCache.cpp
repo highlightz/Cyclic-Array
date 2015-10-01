f07d29a73f863bf03c2334335ef6e062c6bb865b
Highlight Clark <highlightz@163.com>
2015-10-01 10:02:36 +0800

    Update DataCache.cpp

--- DataCache.cpp
+++ DataCache.cpp
@@ -1,51 +1,51 @@
 template < class DataType, int CAPACITY >
-DataCache< DataType >::DataCache( )
+DataCache< DataType, CAPACITY >::DataCache( )
 {
-    lastIndex = -1;
-    numDatas = 0;
+	lastIndex = -1;
+	numDatas = 0;
 }
 
 template < class DataType, int CAPACITY >
-void DataCache< DataType >::writeData( const DataType& newData )
+void DataCache< DataType, CAPACITY >::writeData( const DataType& newData )
 {
-    lastIndex++;
-    if ( lastIndex == CAPACITY )
-    {
-        lastIndex = 0;
-    }
+	lastIndex++;
+	if ( lastIndex == CAPACITY )
+	{
+		lastIndex = 0;
+	}
 
-    lastDatas[lastIndex] = newData;
+	lastDatas[lastIndex] = newData;
 
-    if ( numDatas != CAPACITY )
-    {
-        numDatas++;
-    }
+	if ( numDatas != CAPACITY )
+	{
+		numDatas++;
+	}
 }
 
 template < class DataType, int CAPACITY >
-DataType DataCache< DataType >::getLastData( ) const
+DataType DataCache< DataType, CAPACITY >::getLastData( ) const
 {
-    return lastDatas[ lastIndex ];
+	return lastDatas[ lastIndex ];
 }
 
 // getLastDatas returns up to CAPACITY datas in a DataInfo struct;
 // the number of datas is also in the DataInfo struct.
 // Datas in the datas array are stored in order, with the latest data first
 template< class DataType, int CAPACITY >
-DataInfo< DataType > DataCache< DataType >::getLastDatas( ) const
+DataInfo< DataType, CAPACITY > DataCache< DataType, CAPACITY >::getLastDatas( ) const
 {
-    DataInfo< DataType > info;
-    info.numDatas = numDatas;
+	DataInfo< DataType, CAPACITY > info;
+	info.numDatas = numDatas;
 
-    for ( int i = 0, j = lastIndex; i < numDatas; i++, j-- )
-    {
-        if ( j == -1 )
-        {
-            j = CAPACITY - 1;
-        }
+	for ( int i = 0, j = lastIndex; i < numDatas; i++, j-- )
+	{
+		if ( j == -1 )
+		{
+			j = CAPACITY - 1;
+		}
 
-        info.datas[ i ] = lastDatas[ j ];
-    }
+		info.datas[ i ] = lastDatas[ j ];
+	}
 
-    return info;
+	return info;
 }
